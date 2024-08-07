302f262e205f1f3e42c6ef71c106cc64f5296d4e
liciolentimo <lentimo@gmail.com>
2024-08-07 22:40:27 +0300

    added chaptter schema and layout

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -9,6 +9,7 @@ import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
 import { AttachmentForm } from "./_components/attachment-form";
+import { ChaptersForm } from "./_components/chapters-form";
 
 const CourseIdPage = async ({
     params
@@ -21,9 +22,15 @@ const CourseIdPage = async ({
     }
     const course = await db.course.findUnique({
         where:{
-            id: params.courseId
+            id: params.courseId,
+            userId,
         },
         include: {
+            chapters: {
+                orderBy: {
+                    position: "asc",
+                },
+            },
             attachments: {
                 orderBy: {
                     createdAt: "desc",
@@ -44,7 +51,8 @@ const CourseIdPage = async ({
         course.description,
         course.imageUrl,
         course.price,
-        course.categoryId
+        course.categoryId,
+        course.chapters.some(chapter => chapter.isPublished),
     ];
     const totalFields = requiredFields.length;
     const completedFields = requiredFields.filter(Boolean).length;
@@ -95,9 +103,9 @@ const CourseIdPage = async ({
                         Course chapters
                     </h2>
                     </div>
-                    <div>
-                        TODO: Chapters
-                    </div>
+                    <ChaptersForm
+                initialData={course}
+                courseId={course.id} />
                 </div>
                 <div>
                     <div className="flex items-center gap-x-2">
