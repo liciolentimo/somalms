e0cac7346c08644964e3cfacb6cdc0b3acb3c15d
liciolentimo <lentimo@gmail.com>
2024-08-07 13:57:48 +0300

    added image functionality

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form.tsx
@@ -13,11 +13,10 @@ import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { Textarea } from "@/components/ui/textarea";
+import { Course } from "@prisma/client";
 
 interface DescriptionFormProps {
-    initialData: {
-        description: string;
-    };
+    initialData: Course;
     courseId: string;
 };
 
@@ -36,7 +35,9 @@ export const DescriptionForm = ({
     const router = useRouter();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
-        defaultValues: initialData,
+        defaultValues: {
+            description: initialData?.description || ""
+        },
     });
     const { isSubmitting,isValid} = form.formState;
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
