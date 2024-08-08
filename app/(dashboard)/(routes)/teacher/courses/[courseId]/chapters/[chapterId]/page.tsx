138458b662f06360b016037b579dc52961b07719
liciolentimo <lentimo@gmail.com>
2024-08-08 20:11:16 +0300

    added edit chapter description section

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -5,6 +5,7 @@ import { ArrowLeft, LayoutDashboard } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { ChapterTitleForm } from "./_components/chapter-title-form";
+import { ChapterDescriptionForm } from "../../_components/chapter-description-form";
 
 const ChapterIdPage = async ({
     params
@@ -70,6 +71,10 @@ const ChapterIdPage = async ({
                 initialData={chapter}
                 courseId={params.courseId}
                 chapterId={params.chapterId} />
+                <ChapterDescriptionForm 
+                initialData={chapter}
+                courseId={params.courseId}
+                chapterId={params.chapterId}/>
             </div>
             </div>
             </div>
