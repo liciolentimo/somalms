e195a9804622a19c4025e4964b732139cdd7ba8f
liciolentimo <lentimo@gmail.com>
2024-08-08 23:44:08 +0300

    added upload video functionality

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { ImageIcon, Pencil, PlusCircle, Video } from "lucide-react";
+import { useState } from "react";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Textarea } from "@/components/ui/textarea";
+import { Chapter, Course, MuxData } from "@prisma/client";
+import Image from "next/image";
+import { FileUpload } from "@/components/file-upload";
+import MuxPlayer from "@mux/mux-player-react";
+
+interface ChapterVideoProps {
+    initialData: Chapter & {muxData?: MuxData | null};
+    courseId: string;
+    chapterId: string;
+};
+
+const formSchema = z.object({
+    videoUrl: z.string().min(1),
+});
+
+export const ChapterVideo = ({
+    initialData,
+    courseId,
+    chapterId
+}: ChapterVideoProps) => {
+    const [isEditing,setIsEditing] = useState(false);
+    const toggleEdit = () => setIsEditing((current) => !current);
+    const router = useRouter();
+    // const form = useForm<z.infer<typeof formSchema>>({
+    //     resolver: zodResolver(formSchema),
+    //     defaultValues: {
+    //         imageUrl: initialData?.imageUrl || "",
+    //     },
+    // });
+    //const { isSubmitting,isValid} = form.formState;
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}`,values);
+            toast.success("Chapter updated");
+            toggleEdit();
+            router.refresh();
+        } catch {
+            toast.error("Something went wrong");
+        }
+    }
+    return (
+        <div className="mt-6 border bg-slate-100 rounded-md p-4">
+            <div className="font-medium flex items-center justify-between">
+            Chapter video 
+            <Button onClick={toggleEdit} variant="ghost">
+                {isEditing && (
+                    <>Cancel</>
+                )}
+                {!isEditing && !initialData.videoUrl && (
+                    <>
+                    <PlusCircle className="h-4 w-4 mr-2" />
+                    Add a video
+                    </>
+                )}
+                {!isEditing && initialData.videoUrl && (
+                    <>
+                
+                <Pencil className="h-4 w-4 mr-2" />
+                Edit video 
+                </>
+                )}
+            </Button>
+            </div>
+            {!isEditing && (
+                !initialData.videoUrl ? (
+                    <div className="flex items-center justify-center h-60 bg-slate-200 rounded-md">
+                        <Video className="h-10 w-10 text-slate-500" />
+                    </div>
+                ) : (
+                    <div className="relative aspect-video mt-2">
+                       <MuxPlayer
+                       playbackId={initialData?.muxData?.playbackId || ""} />
+                    </div>
+                )
+            )}
+            {isEditing && (
+                <div>
+                    <FileUpload 
+                    endpoint="chapterVideo"
+                    onChange={(url) => {
+                        if (url) {
+                            onSubmit({videoUrl: url});
+                        }
+                    }}/>
+                    <div className="text-xs text-muted-foreground mt-4">
+                    Upload this chapter's video
+                    </div>
+                </div>
+            )}
+            {initialData.videoUrl && !isEditing && (
+                <div className="text-xs text-muted-foreground mt-2">
+                    Videos can take a few minutes to process. Refresh the page if video does not appear.
+                </div>
+            )}
+        </div>
+    )
+}
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,12 +1,13 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
-import { ArrowLeft, Eye, LayoutDashboard } from "lucide-react";
+import { ArrowLeft, Eye, LayoutDashboard, Video } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { ChapterTitleForm } from "./_components/chapter-title-form";
-import { ChapterDescriptionForm } from "../../_components/chapter-description-form";
-import { ChapterAccessForm } from "../../_components/chapter-access-form";
+import { ChapterDescriptionForm } from "./_components/chapter-description-form";
+import { ChapterAccessForm } from "./_components/chapter-access-form";
+import { ChapterVideo } from "./_components/chapter-video-form";
 
 const ChapterIdPage = async ({
     params
@@ -77,6 +78,7 @@ const ChapterIdPage = async ({
                 courseId={params.courseId}
                 chapterId={params.chapterId}/>
             </div>
+            <div>
             <div className="flex items-center gap-x-2">
             <IconBadge icon={Eye} />
             <h2 className="text-xl">
@@ -89,7 +91,21 @@ const ChapterIdPage = async ({
             chapterId={params.chapterId} />
             </div>
             </div>
+            <div>
+                <div className="flex items-center gap-x-2">
+                <IconBadge icon={Video} />
+                <h2 className="text-xl">
+                    Add a video
+                </h2>
+                </div>
+                <ChapterVideo
+                initialData={chapter}
+                chapterId={params.chapterId}
+                courseId={params.courseId} />
+            </div>
+            </div>
         </div>
+
     );
 }
 
