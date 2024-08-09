fbd2d90f391e2afe01832d77388ff0e158fac16e
liciolentimo <lentimo@gmail.com>
2024-08-09 12:23:35 +0300

    added delete functionality

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -8,6 +8,8 @@ import { ChapterTitleForm } from "./_components/chapter-title-form";
 import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 import { ChapterAccessForm } from "./_components/chapter-access-form";
 import { ChapterVideo } from "./_components/chapter-video-form";
+import { Banner } from "@/components/banner";
+import { ChapterActions } from "./_components/chapter-actions";
 
 const ChapterIdPage = async ({
     params
@@ -38,7 +40,14 @@ const ChapterIdPage = async ({
     const totalFields = requiredFields.length;
     const completedFields = requiredFields.filter(Boolean).length;
     const completionText = `(${completedFields}/${totalFields})`;
+    const isComplete = requiredFields.every(Boolean);
     return (
+        <>
+        {!chapter.isPublished && (
+            <Banner
+            variant="warning"
+            label="This chapter is unpublished. It will not be visible in the course." />
+        )}
         <div className="p-6">
             <div className="flex items-center justify-between">
             <div className="w-full">
@@ -57,6 +66,11 @@ const ChapterIdPage = async ({
                 Complete all fields {completionText}
             </span>
             </div>
+            <ChapterActions
+            disabled={!isComplete}
+            courseId={params.courseId}
+            chapterId={params.chapterId}
+            isPublished={chapter.isPublished} />
             </div>
             </div>
             </div>
@@ -105,7 +119,7 @@ const ChapterIdPage = async ({
             </div>
             </div>
         </div>
-
+        </>
     );
 }
 
