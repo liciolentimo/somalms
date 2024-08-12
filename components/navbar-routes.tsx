bbd0284f362504e8431cd18fcb669fe76a829d22
liciolentimo <lentimo@gmail.com>
2024-08-12 14:04:54 +0300

    search query functionality

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -5,13 +5,21 @@ import { usePathname, useRouter } from "next/navigation";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
+import { SearchInput } from "./search-input";
 
 export const NavbarRoutes = () => {
     const pathname = usePathname();
     //const router = useRouter();
     const isTeacherPage = pathname?.startsWith("/teacher");
     const isPlayerPage = pathname?.includes("/chapter");
+    const isSearchPage = pathname === "/search";
     return (
+        <>
+        {isSearchPage && (
+            <div className="hidden md:block">
+                <SearchInput />
+            </div>
+        )}
         <div className="flex gap-x-2 ml-auto">
             {isTeacherPage || isPlayerPage ? (
                 <Link href="/">
@@ -27,5 +35,6 @@ export const NavbarRoutes = () => {
             )}
             <UserButton />
         </div>
+        </>
     )
 }
