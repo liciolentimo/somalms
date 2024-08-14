9567637e29149a69b75125fec273c19426796cfb
liciolentimo <lentimo@gmail.com>
2024-08-14 11:20:09 +0300

    mobile navbar for chapters

--- app/(course)/courses/[courseId]/layout.tsx
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -4,6 +4,7 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
 import { CourseSidebar } from "./_components/course-sidebar";
+import { CourseNavbar } from "./_components/course-navbar";
 
 const CourseLayout = async ({
     children,
@@ -43,12 +44,17 @@ const CourseLayout = async ({
     const progressCount = await getProgress(userId, course.id);
     return ( 
         <div className="h-full">
+            <div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-50">
+            <CourseNavbar
+            course={course}
+            progressCount={progressCount} />
+            </div>
             <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
             <CourseSidebar
             course={course}
             progressCount={progressCount} />
             </div>
-            <main className="md:pl-80 h-full">
+            <main className="md:pl-80 h-full pt-[80px]">
             {children}
             </main>
         </div>
