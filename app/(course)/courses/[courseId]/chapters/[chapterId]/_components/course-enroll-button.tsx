ac83e680d1cf8de6f9d23f44f7440ab83adaee85
liciolentimo <lentimo@gmail.com>
2024-08-30 16:37:17 +0300

    added stripe checkout

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -2,6 +2,9 @@
 
 import { Button } from "@/components/ui/button";
 import { formatPrice } from "@/lib/format";
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface CourseEnrollButtonProps {
     price: number;
@@ -12,8 +15,22 @@ export const CourseEnrollButton = ({
     price,
     courseId,
 }: CourseEnrollButtonProps) => {
+    const [isLoading,setIsLoading] = useState(false);
+    const onClick = async() => {
+        try {
+            setIsLoading(true);
+            const response = await axios.post(`/api/courses/${courseId}/checkout`);
+            window.location.assign(response.data.url);
+        } catch {
+            toast.error("Something went wrong");
+        } finally {
+            setIsLoading(false);
+        }
+    }
     return (
         <Button
+        onClick={onClick}
+        disabled={isLoading}
          className="w-full md:w-auto"
          size="sm">
             Enroll for {formatPrice(price)}
